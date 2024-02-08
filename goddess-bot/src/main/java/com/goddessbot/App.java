3f0fa35ad173e8a294a754368d872375c5e2c1ff
God0nlyKnows <37994631+God0nlyKnows@users.noreply.github.com>
2024-02-09 00:04:53 +0100

    Revert "add websocket"
    This reverts commit 6c2ed93fd3b3fa7c2658d75da345ba401c7a2e23.

--- goddess-bot/src/main/java/com/goddessbot/App.java
+++ goddess-bot/src/main/java/com/goddessbot/App.java
@@ -1,7 +1,5 @@
 package com.goddessbot;
 
-import com.goddessbot.websocket.WebSocketServer;
-
 import net.dv8tion.jda.api.JDA;
 
 public class App {
@@ -10,17 +8,9 @@ public class App {
 
     public static void main(String[] args) {
         try {
-            Thread webSocketThread = new Thread(() -> {
-                WebSocketServer.startWebSocketServer();
-            });
-            webSocketThread.start();
-
-            // Build and start the JDA Discord bot in the main thread
             jda = Startup.buildJda();
-            jda.awaitReady();
 
-            // Optionally, you can join the WebSocket thread if needed
-            webSocketThread.join();
+            jda.awaitReady();
 
         } catch (Exception e) {
             e.printStackTrace();
